3935298cd107198e32549fae006016b86b3263f0
AyaSagimbek <sagimbekayaulym@gmail.com>
2025-05-03 02:28:05 +0000

    hh

---
+++ ec2.tf
@@ -0,0 +1,32 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] 
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "group1-ubuntu-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  key_name = aws_key_pair.deployer.key_name
+  instance_type = var.instance_type
+  subnet_id = aws_subnet.main.id
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+
+  tags = {
+    Name = "group-1"
+  }
+}
---
+++ sg.tf
@@ -0,0 +1,29 @@
+resource "aws_security_group" "allow_tls" {
+  name        = "group-1"
+  description = "Allow TLS inbound traffic"
+  vpc_id = aws_vpc.group-1.id
+
+  ingress {
+    description      = "http"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+
+    ingress {
+    description      = "ssh"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
