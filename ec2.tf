b4969bdfc1dbfffde7c6cec4743bc5f6f1ef7f2e
AyaSagimbek <sagimbekayaulym@gmail.com>
2025-05-03 03:43:47 +0000

    n

--- ec2.tf
+++ ec2.tf
@@ -21,7 +21,7 @@ resource "aws_key_pair" "deployer1" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
-  key_name = aws_key_pair.deployer.key_name
+  key_name = aws_key_pair.deployer1.key_name
   instance_type = var.instance_type
   subnet_id = aws_subnet.main.id
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
